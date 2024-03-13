46a74e36a3516b688207a981c936d6041373d7d3
samuelhssutbr <161604388+samuelhssutbr@users.noreply.github.com>
2024-03-13 08:47:34 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes=document:querySelectorAll("botao");
+flor(let i=0;<botoes.length;i++){
+botoes.onclick=functiom(){
+
+  for(let j-0;j<botoes.lenght;j++{
+    botoes.classlist.remove("ativo");
+  }
+  botoes[i].classlist.add("ativo");
+}
+}
