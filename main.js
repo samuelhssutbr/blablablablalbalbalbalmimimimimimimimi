436f8d34de390886866973b4d7ccdaa6eb379908
samuelhssutbr <161604388+samuelhssutbr@users.noreply.github.com>
2024-03-13 08:48:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes=document:querySelectorAll("botao");
-flor(let i=0;<botoes.length;i++){
+for(let i=0;<botoes.length;i++){
 botoes.onclick=functiom(){
 
   for(let j-0;j<botoes.lenght;j++{
