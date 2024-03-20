23d041a81dad226a1b873e70635e138c71451a54
samuelhssutbr <161604388+samuelhssutbr@users.noreply.github.com>
2024-03-20 08:12:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const botoes=document:querySelectorAll("botao");
-for(let i=0;<botoes.length;i++){
-botoes.onclick=functiom(){
+const botoes = document.querySelectorAll(".botao");
 
-  for(let j-0;j<botoes.lenght;j++{
-    botoes.classlist.remove("ativo");
-  }
-  botoes[i].classlist.add("ativo");
-}
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
 }
